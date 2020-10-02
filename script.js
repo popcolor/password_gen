632c780537501c04022d38d8cdb4587dfeea0ee9
popcolor <davidc453@gmail.com>
2020-10-02 13:40:57 -0400

    update

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+// var password = generatePassword();
+//var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+//passwordText.value = password;
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword());
+//var generateBtn = document.querySelector("#generate");
+
+var charAmount = prompt(
+  "choose a length of at least 8 characters and no more than 128 characters"
+);
+console.log(charAmount);
+
+var lowercase = confirm("Would you like to include lowercase letters?");
+console.log(lowercase);
+var uppercase = confirm("Would you like to include uppercase letters?");
+console.log(uppercase);
+var numeric = confirm("Would you like numerical values?");
+console.log(numeric);
+var special = confirm("Would you like to include special characters?");
+console.log(special);
