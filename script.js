176b29332613226177e4db53672667e8a79535c1
popcolor <davidc453@gmail.com>
2020-10-02 16:13:12 -0400

    updated while loop

--- script.js
+++ script.js
@@ -13,10 +13,18 @@
 //generateBtn.addEventListener("click", writePassword());
 //var generateBtn = document.querySelector("#generate");
 
-var charAmount = prompt(
+var length = prompt(
   "choose a length of at least 8 characters and no more than 128 characters"
 );
-console.log(charAmount);
+
+while (true) {
+  var length = prompt(
+    "choose a length of at least 8 characters and no more than 128 characters"
+  );
+  if (length >= 8 && length <= 128) {
+    break;
+  }
+}
 
 var lowercase = confirm("Would you like to include lowercase letters?");
 console.log(lowercase);
@@ -26,3 +34,15 @@ var numeric = confirm("Would you like numerical values?");
 console.log(numeric);
 var special = confirm("Would you like to include special characters?");
 console.log(special);
+
+function stringGen(len) {
+  var text = " ";
+
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < len; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
+alert(stringGen(length));
