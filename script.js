f801267b804186626c24dbf16922604772ab5f4b
popcolor <davidc453@gmail.com>
2020-10-03 16:53:34 -0400

    updated functions

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", genBtn);
 
-// Write password to the #password input
-//function writePassword() {
-// var password = generatePassword();
-//var passwordText = document.querySelector("#password");
-
-//passwordText.value = password;
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword());
-//var generateBtn = document.querySelector("#generate");
-
-var length = prompt(
-  "choose a length of at least 8 characters and no more than 128 characters"
-);
-
-while (true) {
-  var length = prompt(
-    "choose a length of at least 8 characters and no more than 128 characters"
-  );
-  if (length >= 8 && length <= 128) {
-    break;
+function genBtn() {
+  while (true) {
+    var promptLength = prompt(
+      "choose a length of at least 8 characters and no more than 128 characters"
+    );
+    if (promptLength >= 8 && promptLength <= 128) {
+      break;
+    }
   }
+
+  var lowercaseAnswer = confirm("Would you like to include lowercase letters?");
+  console.log(lowercaseAnswer);
+  var uppercaseAnswer = confirm("Would you like to include uppercase letters?");
+  console.log(uppercaseAnswer);
+  var numericAnswer = confirm("Would you like numerical values?");
+  console.log(numericAnswer);
+  var specialAnswer = confirm("Would you like to include special characters?");
+  console.log(specialAnswer);
+  var settings = {
+    LC: lowercaseAnswer,
+    UC: uppercaseAnswer,
+    NM: numericAnswer,
+    SA: specialAnswer,
+    PL: promptLength,
+  };
+  console.log(settings);
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "~`!@#$%^&*()_+-={}[]";
+
+  function makeid() {
+    var text = "";
+    var possible = "";
+
+    if (settings.LC) {
+      possible += lowercase;
+    }
+    if (settings.UC) {
+      possible += uppercase;
+    }
+    if (settings.NM) {
+      possible += numbers;
+    }
+    if (settings.SA) {
+      possible += special;
+    }
+
+    for (var i = 0; i < settings.PL; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    alert(text);
+    console.log(text.length);
+
+    return text;
+  }
+
+  makeid();
 }
 
-var lowercase = confirm("Would you like to include lowercase letters?");
-console.log(lowercase);
-var uppercase = confirm("Would you like to include uppercase letters?");
-console.log(uppercase);
-var numeric = confirm("Would you like numerical values?");
-console.log(numeric);
-var special = confirm("Would you like to include special characters?");
-console.log(special);
 
-function stringGen(len) {
-  var text = " ";
-
-  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < len; i++)
-    text += charset.charAt(Math.floor(Math.random() * charset.length));
-
-  return text;
-}
-alert(stringGen(length));
